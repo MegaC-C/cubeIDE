946baf92031de7582a97e7fd1f678f78d8583d92
corvi <corvi@192.168.0.17>
2021-01-23 19:11:53 +0100

    first commit

---
+++ F373RC_SDADC_test/Core/Src/stm32f3xx_hal_msp.c
@@ -0,0 +1,145 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file         stm32f3xx_hal_msp.c
+  * @brief        This file provides code for the MSP Initialization
+  *               and de-Initialization codes.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN Define */
+
+/* USER CODE END Define */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN Macro */
+
+/* USER CODE END Macro */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* External functions --------------------------------------------------------*/
+/* USER CODE BEGIN ExternalFunctions */
+
+/* USER CODE END ExternalFunctions */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+/**
+  * Initializes the Global MSP.
+  */
+void HAL_MspInit(void)
+{
+  /* USER CODE BEGIN MspInit 0 */
+
+  /* USER CODE END MspInit 0 */
+
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* System interrupt init*/
+
+  /* USER CODE BEGIN MspInit 1 */
+
+  /* USER CODE END MspInit 1 */
+}
+
+/**
+* @brief SDADC MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hsdadc: SDADC handle pointer
+* @retval None
+*/
+void HAL_SDADC_MspInit(SDADC_HandleTypeDef* hsdadc)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hsdadc->Instance==SDADC3)
+  {
+  /* USER CODE BEGIN SDADC3_MspInit 0 */
+
+  /* USER CODE END SDADC3_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_SDADC3_CLK_ENABLE();
+
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    /**SDADC3 GPIO Configuration
+    PD8     ------> SDADC3_AIN6P
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN SDADC3_MspInit 1 */
+
+  /* USER CODE END SDADC3_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief SDADC MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hsdadc: SDADC handle pointer
+* @retval None
+*/
+void HAL_SDADC_MspDeInit(SDADC_HandleTypeDef* hsdadc)
+{
+  if(hsdadc->Instance==SDADC3)
+  {
+  /* USER CODE BEGIN SDADC3_MspDeInit 0 */
+
+  /* USER CODE END SDADC3_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SDADC3_CLK_DISABLE();
+
+    /**SDADC3 GPIO Configuration
+    PD8     ------> SDADC3_AIN6P
+    */
+    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_8);
+
+  /* USER CODE BEGIN SDADC3_MspDeInit 1 */
+
+  /* USER CODE END SDADC3_MspDeInit 1 */
+  }
+
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
